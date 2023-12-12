aab8c1ee26f3b91943f0c591ed18e5a44f92d7f8
user\user2 <tianlx@ying31.com>
2023-12-12 11:06:16 +0800

    test:新增webhook判斷

--- Services/ConfigureWebhook.cs
+++ Services/ConfigureWebhook.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace Telegram.Bot.Services;
@@ -8,6 +10,7 @@ public class ConfigureWebhook : IHostedService
     private readonly ILogger<ConfigureWebhook> _logger;
     private readonly IServiceProvider _serviceProvider;
     private readonly BotConfiguration _botConfig;
+    private bool ExistWebhook;
 
     public ConfigureWebhook(
         ILogger<ConfigureWebhook> logger,
@@ -21,29 +24,65 @@ public class ConfigureWebhook : IHostedService
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        using var scope = _serviceProvider.CreateScope();
-        var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+        await ExistWebhookAsync();
 
-        // Configure custom endpoint per Telegram API recommendations:
-        // https://core.telegram.org/bots/api#setwebhook
-        // If you'd like to make sure that the webhook was set by you, you can specify secret data
-        // in the parameter secret_token. If specified, the request will contain a header
-        // "X-Telegram-Bot-Api-Secret-Token" with the secret token as content.
-        var webhookAddress = $"{_botConfig.HostAddress}{_botConfig.Route}";
-        _logger.LogInformation("Setting webhook: {WebhookAddress}", webhookAddress);
-        //await botClient.SetWebhookAsync(
-        //    url: webhookAddress,
-        //    allowedUpdates: Array.Empty<UpdateType>(),
-        //    cancellationToken: cancellationToken);
+        if(!ExistWebhook)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+            // Configure custom endpoint per Telegram API recommendations:
+            // https://core.telegram.org/bots/api#setwebhook
+            // If you'd like to make sure that the webhook was set by you, you can specify secret data
+            // in the parameter secret_token. If specified, the request will contain a header
+            // "X-Telegram-Bot-Api-Secret-Token" with the secret token as content.
+            var webhookAddress = $"{_botConfig.HostAddress}{_botConfig.Route}";
+            _logger.LogInformation("Setting webhook: {WebhookAddress}", webhookAddress);
+
+            await botClient.SetWebhookAsync(
+                url: webhookAddress,
+                allowedUpdates: Array.Empty<UpdateType>(),
+                cancellationToken: cancellationToken);
+        }
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
+        if (!ExistWebhook)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+            // Remove webhook on app shutdown
+            _logger.LogInformation("Removing webhook");
+            await botClient.DeleteWebhookAsync(cancellationToken: cancellationToken);
+        }
+
+    }
+
+    public async Task ExistWebhookAsync()
+    {
         using var scope = _serviceProvider.CreateScope();
         var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+        _logger.LogInformation("判斷Webhook是否已設定");
+        ExistWebhook = false;
 
-        // Remove webhook on app shutdown
-        _logger.LogInformation("Removing webhook");
-        //await botClient.DeleteWebhookAsync(cancellationToken: cancellationToken);
+        try
+        {
+            WebhookInfo webhookInfo = await botClient.GetWebhookInfoAsync();
+
+            if (!string.IsNullOrEmpty(webhookInfo.Url))
+            {
+                _logger.LogInformation($"Webhook is set. URL: {webhookInfo.Url}");
+                ExistWebhook = true;
+            }
+
+            _logger.LogInformation("Webhook is not set.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+        }
+
     }
 }
